aba6e084c16ac6929fa320838459111a4530a27f
Liz Kaufman <lizkaufman92@gmail.com>
2023-01-12 11:52:55 +0000

    setting up session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 //In Next.js, you can add global CSS files by importing them from pages/_app.js. You cannot import global CSS anywhere else.
 // Any styles imported in _app.js will be applied globally, to all pages of the application.
+import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const { session } = pageProps;
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
+
 import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
+
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
+  // getStaticProps only runs on the server-side. It will never run on the client-side. It won’t even be included in the JS bundle for the browser. That means you can write code such as direct database queries without them being sent to browsers.
+  // Because it’s meant to be run at build time, you won’t be able to use data that’s only available during request time, such as query parameters or HTTP headers.
+  // getStaticProps can only be exported from a page. You can’t export it from non-page files.
+  //Alternative: getServerSideProps (https://nextjs.org/learn/basics/data-fetching/request-time)
   const allPostsData = getSortedPostsData();
   return {
     props: {
@@ -14,12 +21,10 @@ export async function getStaticProps() {
   };
 }
 
-// getStaticProps only runs on the server-side. It will never run on the client-side. It won’t even be included in the JS bundle for the browser. That means you can write code such as direct database queries without them being sent to browsers.
-// Because it’s meant to be run at build time, you won’t be able to use data that’s only available during request time, such as query parameters or HTTP headers.
-// getStaticProps can only be exported from a page. You can’t export it from non-page files.
-//Alternative: getServerSideProps (https://nextjs.org/learn/basics/data-fetching/request-time)
-
 export default function Home({ allPostsData }) {
+  const { data, status } = useSession();
+  const userEmail = session?.user.email;
+
   return (
     <Layout home>
       <Head>
@@ -32,20 +37,30 @@ export default function Home({ allPostsData }) {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+
+      {status === "authenticated" ? (
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <p>Signed in as {userEmail}</p>
+          <button onClick={() => signOut()}>Sign out</button>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+      ) : (
+        <>
+          <p>Not signed in.</p>
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
     </Layout>
   );
 }
